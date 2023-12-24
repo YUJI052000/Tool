f335a24a1c9ed88078694bbf5778b54179f9c6a7
YUJI052000 <149587873+YUJI052000@users.noreply.github.com>
2023-12-24 17:34:37 +0800

    Update Style.py

--- Style.py
+++ Style.py
@@ -2,4 +2,4 @@ from rich.panel import Panel as nel
 from rich import print as cetak
 
 statusok = f'[•] USER ID : djjd\n[•] PASSWORD : jsjd\n[•] FB LINK : https://www.facebook.com/hshs\n[•] FOLLOWERS : 2628\n[•] CREATION : yeie'
-cetak(nel(statusok, title=f'YUJI-OK'))
+cetak(nel(statusok, title=f'YUJI-OK')style='green')
