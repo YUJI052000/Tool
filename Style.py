7037d2c847026860fb965c18f551c19dec7e326f
YUJI052000 <149587873+YUJI052000@users.noreply.github.com>
2023-12-24 16:59:15 +0800

    Create Style.py

---
+++ Style.py
@@ -0,0 +1,5 @@
+from rich.panel import Panel as nel
+from rich import print as cetak
+
+statusok = f'[•] USER ID : djjd\n[•] PASSWORD : jsjd\n[•] FB LINK : https://www.facebook.com/hshs\n[•] FOLLOWERS : 2628\n[•] CREATION : yeie'
+statusok1 = nel(statusok, style='green')
