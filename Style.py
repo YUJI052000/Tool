6b38b60547010f7bf80335f8fbd15d53d7f5e505
YUJI052000 <149587873+YUJI052000@users.noreply.github.com>
2023-12-24 17:32:28 +0800

    Update Style.py

--- Style.py
+++ Style.py
@@ -3,4 +3,3 @@ from rich import print as cetak
 
 statusok = f'[•] USER ID : djjd\n[•] PASSWORD : jsjd\n[•] FB LINK : https://www.facebook.com/hshs\n[•] FOLLOWERS : 2628\n[•] CREATION : yeie'
 statusok = nel(statusok, style='green')
-cetak(nel(statusok, title=f'YUJI-OK'))
