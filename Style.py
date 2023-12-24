975279586ebbbd381201a1e5e297331ea9708127
YUJI052000 <149587873+YUJI052000@users.noreply.github.com>
2023-12-24 17:31:49 +0800

    Update Style.py

--- Style.py
+++ Style.py
@@ -2,4 +2,5 @@ from rich.panel import Panel as nel
 from rich import print as cetak
 
 statusok = f'[•] USER ID : djjd\n[•] PASSWORD : jsjd\n[•] FB LINK : https://www.facebook.com/hshs\n[•] FOLLOWERS : 2628\n[•] CREATION : yeie'
+statusok = nel(statusok, style='green')
 cetak(nel(statusok, title=f'YUJI-OK'))
